c862d7fb6e3addd789363921b497f7ab7660b853
ZanaDrag <kirachis73@gmail.com>
2026-06-08 16:39:42 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const fs      = require('fs');
 const path    = require('path');
 
 const app  = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 /* ──────────────────────────────────────────────
    Статические файлы (HTML / CSS / JS / images)
